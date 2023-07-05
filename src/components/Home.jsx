08b93704713b07cf143cc19764ecaa05234d9f0c
Suryansh <saranshkalia95@gmail.com>
2023-07-05 15:43:48 +0530

    add route to TypesOfCancer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import imgWide from "./../images/cancer-gov-hero-widescreen-202301.jpg";
 import cancerSideEffects from "./../images/1000_F_316738552_vU8mlm1Ag97kKuwy9G2ax1Hrhw5kaM1M.jpg";
-import cancerTreatmentEffects from "./../images/xNausea_CWCrop.jpg.resize.jpg.pagespeed.ic.nBLKh-n7kV.jpg";
 
-const Card = ({ imageSrc, title, description, src }) => {
+const Card = ({ imageSrc, title, description, link }) => {
   return (
-    <div className="card flex flex-1  shadow-xl p-4 mb-4">
-      <Link to={src}>
-        <img src={imageSrc} alt="Card" className="h-60 w-auto" />
+    <div className="container text-center w-auto bg-gray-300 shadow-2xl p-4 mb-4">
+      <Link to={link}>
+        <img src={imageSrc} alt="Card" className="h-80 w-auto" />
         <h3>{title}</h3>
         <p>{description}</p>
       </Link>
@@ -19,7 +18,7 @@ const Card = ({ imageSrc, title, description, src }) => {
 
 const Links = ({ links, title, src }) => {
   return (
-    <div className="card-container flex flex-1 flex-col shadow-xl p-4 mb-4 ">
+    <div className="card-container flex flex-col shadow-xl p-4 mb-4">
       <h3 className="mb-5">{title}</h3>
       <ul>
         {links.map((link, index) => (
@@ -38,39 +37,42 @@ const Home = () => {
     "Causes & prevention",
     "Diagnoses & staging",
   ];
-  const src = ["about", "/causes-prevention", "/diagnoses-staging"];
+  const src = ["/what-is-cancer", "/causes-prevention", "/diagnoses-staging"];
 
   return (
     <React.Fragment>
-      <main className="flex flex-col gap-10 ">
-        <section className="container mx-auto">
-          <img src={imgWide} alt="cancer-gov-hero" className="w-full" />
+      <main className="flex flex-col gap-10">
+        <section className="container flex justify-center mx-auto">
+          <img
+            src={imgWide}
+            alt="cancer-gov-hero"
+            className="shadow-2xl m-4"
+          />
         </section>
-        <section className="container mx-auto flex justify-between">
+        <section className="container gap-24 mx-auto flex justify-between">
           <Card
+            link={"/typesofcancer"}
             imageSrc={cancerSideEffects}
-            title="Side Effects Of Cancer"
+            title="Types of cancer"
             description="This is the description"
-          ></Card>
+          />
           <Card
             imageSrc={cancerSideEffects}
             title="Side Effects of Cancer Treatment"
             description="This is the description"
-          ></Card>
+          />
           <Card
             imageSrc={cancerSideEffects}
-            title="Types of cancer"
+            title="Side effects of cancer"
             description="This is the description"
-          ></Card>
+          />
         </section>
-        <section className="Link container mx-auto flex justify-between">
-          <Links title="About Cancer" links={links} src={src}></Links>
+        <section className="container mx-auto flex justify-between">
+          <Links title="About Cancer" links={links} src={src} />
         </section>
       </main>
     </React.Fragment>
   );
 };
-
-
 
 export default Home;
