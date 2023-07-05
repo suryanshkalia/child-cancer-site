111a8ed3dcc51095dcbece5c95ff84966d06488c
Suryansh <saranshkalia95@gmail.com>
2023-07-05 11:35:54 +0530

    add dropdown to About link

--- src/components/universal/Header.jsx
+++ src/components/universal/Header.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import logo from "./../../images/download.png"
 
 const Header = () => {
+
+  const [showDropdown, setShowDropdown] = useState(false)
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown)
+  }
+
+  const About = [
+    "Our profile",
+    "Contact Information"
+  ];
+  
+
   return (
     <React.Fragment>
       <div>
         <header className="bg-gray-900 p-4 text-white flex justify-between ">
           <div>
-            <Link to="/">
+            <Link to="">
             <img src={logo} alt="cancerLogo" className="h-auto w-16" ></img>
             </Link>
           </div>
@@ -18,7 +31,20 @@ const Header = () => {
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="about">About</Link>
+                <div>
+                  <button onClick={toggleDropdown} className="cursor-pointer focus:outline-none">
+                    About
+                  </button>
+                  {showDropdown && (
+                    <ul className="absolute text-black px-4 bg-white rounded-lg shadow-md">
+                      {About.map((links, index) => (
+                        <li className="my-2">
+                          <Link to="">{links}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
               </li>
               <li>
                 <Link to="services" >Services</Link>
