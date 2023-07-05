8c61ff41a4c214efa20a0a6a2ba00b0a83acbec6
Suryansh <saranshkalia95@gmail.com>
2023-07-05 12:15:20 +0530

    add hover on links

--- src/components/universal/Header.jsx
+++ src/components/universal/Header.jsx
@@ -1,62 +1,78 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "./../../images/download.png"
+import logo from "./../../images/download.png";
 
 const Header = () => {
-
-  const [showDropdown, setShowDropdown] = useState(false)
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const toggleDropdown = () => {
-    setShowDropdown(!showDropdown)
-  }
+    setShowDropdown(!showDropdown);
+  };
 
-  const About = [
-    "Our profile",
-    "Contact Information"
-  ];
-  
+  const About = ["Our profile", "Contact Information"];
 
   return (
-    <React.Fragment>
-      <div>
-        <header className="bg-gray-900 p-4 text-white flex justify-between ">
-          <div>
-            <Link to="">
-            <img src={logo} alt="cancerLogo" className="h-auto w-16" ></img>
-            </Link>
-          </div>
-          <div>
-            <ul className="flex justify-between gap-5">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <div>
-                  <button onClick={toggleDropdown} className="cursor-pointer focus:outline-none">
-                    About
-                  </button>
-                  {showDropdown && (
-                    <ul className="absolute text-black px-4 bg-white rounded-lg shadow-md">
-                      {About.map((links, index) => (
-                        <li className="my-2">
-                          <Link to="">{links}</Link>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-              </li>
-              <li>
-                <Link to="services" >Services</Link>
-              </li>
-              <li>
-                <Link to="contacts">Contacts</Link>
-              </li>
-            </ul>
-          </div>
-        </header>
+    <header className="bg-white shadow-2xl">
+      <div className="container mx-auto py-4 flex justify-between items-center">
+        <Link to="/">
+          <img src={logo} alt="Cancer Logo" className="h-auto w-16" />
+        </Link>
+        <nav>
+          <ul className="flex items-center space-x-6">
+            <li>
+              <Link
+                to="/"
+                className="font-medium text-black-700 hover:text-red-600"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <div className="relative">
+                <button
+                  onClick={toggleDropdown}
+                  className="font-medium text-black-700 hover:text-red-600"
+                  aria-expanded={showDropdown}
+                  aria-haspopup="true"
+                >
+                  About
+                </button>
+                {showDropdown && (
+                  <ul className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-md">
+                    {About.map((link, index) => (
+                      <li key={index}>
+                        <Link
+                          to=""
+                          className="block py-2 px-4 text-gray-700 hover:bg-gray-100"
+                        >
+                          {link}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            </li>
+            <li>
+              <Link
+                to="/services"
+                className="font-medium text-black-700 hover:text-red-600"
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contacts"
+                className="font-medium text-black-700 hover:text-red-600"
+              >
+                Contacts
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </React.Fragment>
+    </header>
   );
 };
 
