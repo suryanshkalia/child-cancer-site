c92cf89162d520d1d47fc811c2962ec1f1bc287f
Suryansh <saranshkalia95@gmail.com>
2023-07-06 15:45:38 +0530

    adjust padding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import imgWide from "./../images/cancer-gov-hero-widescreen-202301.jpg";
-import cancerSideEffects from "./../images/1000_F_316738552_vU8mlm1Ag97kKuwy9G2ax1Hrhw5kaM1M.jpg";
-import Cancer from "./../images/download.png";
+import childawareness from "./../images/gold-ribbon-childhood-cancer-awareness.jpeg";
+import Cancer from "./../images/breast cancer cell.webp";
 import treatment from "./../images/images.jpeg"
 
 const Card = ({ imageSrc, title, description, link }) => {
@@ -11,7 +11,7 @@ const Card = ({ imageSrc, title, description, link }) => {
     <div className="container text-center w-auto bg-gray-300 shadow-2xl p-4 mb-4">
       <Link to={link}>
         <img src={imageSrc} alt="Card" className="h-80 w-auto" />
-        <h3 className="text-xl" >{title}</h3>
+        <h3 className="text-2xl " >{title}</h3>
       </Link>
     </div>
   );
@@ -21,7 +21,7 @@ const Home = () => {
 
   return (
     <React.Fragment>
-      <main className="flex flex-col gap-10">
+      <main className="flex flex-col gap-10  p-20">
         <section className="container flex justify-center mx-auto">
           <img src={imgWide} alt="cancer-gov-hero" className="shadow-2xl m-4" />
         </section>
@@ -37,9 +37,9 @@ const Home = () => {
             title="Cancer Treatment"
           />
           <Card
-          link={"/awareness"}
-            imageSrc={cancerSideEffects}
-            title="Childhood Cancer Awareness"
+          link={"/childawareness"}
+            imageSrc={childawareness}
+            title="Child Cancer Awareness"
           />
         </section>
       </main>
