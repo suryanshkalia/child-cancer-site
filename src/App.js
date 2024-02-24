48076e28efcd64c3a58479be72b60796ffca5e13
Suryansh <saranshkalia95@gmail.com>
2024-02-24 17:57:16 +0530

    reverting

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
 import Layout from "./components/universal/Layout";
 import Home from "./components/Home";
 import TypesOfCancer from "./components/TypeOfCancers";
 import ServicesPage from "./components/Services";
 import CancerInfo from "./components/CancerInfo";
 import CancerTreatment from "./components/CancerTreatment";
-import childCancerAwareness from "./components/childhoodCancerAwareness";
+import ChildCancerAwareness from "./components/ChildCancerAwareness";
 
 const App = () => {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route  index element={<Home/>}/>
-            <Route path="typesofcancer" element={<TypesOfCancer/>}/>
-            <Route path="services" element={<ServicesPage/>}/>
-            <Route path="cancer" element={<CancerInfo/>}/>
-            <Route path="treatment" element={<CancerTreatment/>}/>
-            <Route path="childcancerawareness" element={<childCancerAwareness/>}/>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="typesofcancer" element={<TypesOfCancer />} />
+            <Route path="services" element={<ServicesPage />} />
+            <Route path="cancer" element={<CancerInfo />} />
+            <Route path="treatment" element={<CancerTreatment />} />
+            <Route
+              path="childcancerawareness"
+              element={<childCancerAwareness />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
